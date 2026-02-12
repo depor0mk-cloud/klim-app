60196a6288c8dadfbc04ca1056396d7ac8915969
depor0mk-cloud <depor0mk@gmail.com>
2026-02-12 07:40:17 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { 
-  getAuth, 
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import {
-  getDatabase,
-  ref,
-  set,
-  push,
-  onValue,
-  onChildAdded,
-  query,
-  orderByChild,
-  limitToLast,
-  serverTimestamp,
-  get
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-// ⚠️ ТВОИ РОДНЫЕ КЛЮЧИ — ВСЁ ЧЕСТНО, НЕ ПИЗЖУ!
 const firebaseConfig = {
   apiKey: "AIzaSyC6Ticcyl0p-zas2cecR2D_e8Ocp4qgrwc",
   authDomain: "klim-74f9f.firebaseapp.com",
@@ -28,8 +9,7 @@ const firebaseConfig = {
   projectId: "klim-74f9f",
   storageBucket: "klim-74f9f.firebasestorage.app",
   messagingSenderId: "168453917755",
-  appId: "1:168453917755:web:62b2e31b895485a4bd601e",
-  measurementId: "G-1N6X0JLNHH"
+  appId: "1:168453917755:web:62b2e31b895485a4bd601e"
 };
 
 const app = initializeApp(firebaseConfig);
